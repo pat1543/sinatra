fc249ac45a952108f052b0084b44d854a07c3182
pat1543 <pat1543@gmail.com>
2019-08-02 09:43:36 +0900

    final

--- memopp/memo.rb
+++ memopp/memo.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
@@ -5,20 +7,19 @@ require 'json'
 require 'securerandom'
 
 before do
-  File.open("articles.json") do |file|
+  File.open('articles.json') do |file|
     @articles = JSON.load(file)
   end
 end
 
 get '/' do
-  File.open("articles.json") do |file|
-    @articles = JSON.load(file)
-  end
   erb :index
 end
 
 get '/show/:id' do
-  @post = @articles["memos"].find{|x| x["id"] == params[:id]}["article"]
+  @id = @articles['memos'].find { |x| x['id'] == params[:id] }['id']
+  @title = @articles['memos'].find { |x| x['id'] == params[:id] }['title']
+  @article = @articles['memos'].find { |x| x['id'] == params[:id] }['article']
   erb :show
 end
 
@@ -26,23 +27,47 @@ get '/new' do
   erb :new
 end
 
+get '/edit/:id' do
+  @id = @articles['memos'].find { |x| x['id'] == params[:id] }['id']
+  @title = @articles['memos'].find { |x| x['id'] == params[:id] }['title']
+  @article = @articles['memos'].find { |x| x['id'] == params[:id] }['article']
+  erb :edit
+end
+
+patch '/edit/:id' do
+  @articles['memos'].find { |x| x['id'] == params[:id] }['title'] = params[:title]
+  @articles['memos'].find { |x| x['id'] == params[:id] }['article'] = params[:article]
+  File.open('articles.json', 'w') do |f|
+    JSON.dump({ 'memos' => @articles['memos'] }, f)
+  end
+  erb :result
+end
+
+delete '/edit/:id' do
+  hash = @articles['memos'].find { |x| x['id'] == params[:id] }
+  @index = @articles['memos'].index(hash)
+  @articles['memos'].delete_at(@index)
+  File.open('articles.json', 'w') do |f|
+    JSON.dump({ 'memos' => @articles['memos'] }, f)
+  end
+  erb :delete_result
+end
+
 class Create
-  attr_reader :id, :article, :body
+  attr_accessor :id, :title, :article
 
   def initialize(title, article)
     @id = SecureRandom.uuid
     @title = title
     @article = article
-
   end
-
 end
 
-  post '/new' do
-    @memo = Create.new(params[:title], params[:article])
-    @articles["memos"] << {id: @memo.id, article: @memo.article}
-    File.open("articles.json", "w") do |f|
-      JSON.dump({"memos" => @articles["memos"]}, f)
-    end
-    erb :result
+post '/new' do
+  @memo = Create.new(params[:title], params[:article])
+  @articles['memos'] << { id: @memo.id, title: @memo.title, article: @memo.article }
+  File.open('articles.json', 'w') do |f|
+    JSON.dump({ 'memos' => @articles['memos'] }, f)
   end
+  erb :result
+end
